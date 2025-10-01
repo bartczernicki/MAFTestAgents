bd0202619e8a55ac526b7006037c94dc045d2a82
Bart Czernicki <bartczernicki@outlook.com>
2025-10-01 16:49:36 -0400

    Workflow

--- MAFTestAgents/Program.cs
+++ MAFTestAgents/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Azure;
 using Azure.AI.OpenAI;
 using Microsoft.Agents.AI;
+using Microsoft.Agents.AI.Workflows;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
 
 namespace MAFTestAgents
 {
@@ -10,6 +12,8 @@ namespace MAFTestAgents
     {
         static async Task Main(string[] args)
         {
+            // https://devblogs.microsoft.com/dotnet/introducing-microsoft-agent-framework-preview/ 
+
             Console.WriteLine("Setting up Config...");
             var configurationBuilder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -30,20 +34,44 @@ namespace MAFTestAgents
             // Retrieve the Chat Client
             var azureOpenAIChatClient = azureOpenAIClient.GetChatClient(azureOpenAIModelDeploymentName).AsIChatClient();
 
-            var agentOptions = new ChatClientAgentOptions
+            var agentWriterOptions = new ChatClientAgentOptions
             {
                 Name = "Writer",
                 Instructions = "Write stories that are engaging and creative."
             };
 
+            var agentEditorOptions = new ChatClientAgentOptions
+            {
+                Name = "Editor",
+                Instructions = "Make the story more engaging, fix grammar, and enhance the plot."
+            };
+
+
             var agentWriter = new ChatClientAgent(
                 azureOpenAIChatClient,
-                agentOptions
+                agentWriterOptions
                 );
 
-            AgentRunResponse response = await agentWriter.RunAsync("Write a short story about a haunted house.");
+            var agentEditor = new ChatClientAgent(
+                azureOpenAIChatClient,
+                agentEditorOptions
+                );
 
-            Console.WriteLine(response.Text);
+            // AgentRunResponse response = await agentWriter.RunAsync("Write a short story about a haunted house.");
+            // Console.WriteLine(response.Text);
+
+            // Create a workflow that connects writer to editor
+            Workflow workflow =
+                AgentWorkflowBuilder
+                    .BuildSequential(agentWriter, agentEditor);
+
+            AIAgent workflowAgent = await workflow.AsAgentAsync();
+
+
+            AgentRunResponse workflowResponse =
+                await workflowAgent.RunAsync("Write a short story about a haunted house.");
+
+            Console.WriteLine(workflowResponse.Text);
         }
     }
 }
